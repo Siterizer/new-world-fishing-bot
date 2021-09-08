0e53d7ec34de85dfb11609fe07ee84eaf6d6262f
Siterizer <siterizer@gmail.com>
2021-09-08 21:53:22 +0200

    use pyautogui instead of win32api from screenshots

--- pyautogui_wrapper.py
+++ pyautogui_wrapper.py
@@ -1,10 +1,13 @@
 from logging_module import *
 from PIL import Image
 import pyautogui
-import win32gui
-import win32ui
-import win32con
 import os
+import numpy as np
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #Disable tensorflow warnings
+from tensorflow import keras
+
+class_names = ['0', '1', '2', '3']
+model = keras.models.load_model("model/")
 
 def click_mouse_with_coordinates(x1, y1):
     log("click: x={} y= {}".format(x1, y1))
@@ -26,29 +29,22 @@ def release_mouse_key():
     log("release mouse key")
     pyautogui.mouseUp()
 
-def get_screenshot(x, y, width, height):
+def screen_recognize(x, y, width, height):
     log("take screenshot: x={} y={} width={} height={}".format(x, y, width, height))
-    actual_time = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')
-    bmpfilenamename = 'saved_data/screenshots/result.bmp'
-    #result = 'saved_data/screenshots/' + actual_time + '.jpg'
-    result = 'saved_data/screenshots/result.jpg'
-    hwnd = win32gui.FindWindow(None, 'tak')
-    wDC = win32gui.GetWindowDC(hwnd)
-    dcObj=win32ui.CreateDCFromHandle(wDC)
-    cDC=dcObj.CreateCompatibleDC()
-    dataBitMap = win32ui.CreateBitmap()
-    dataBitMap.CreateCompatibleBitmap(dcObj, width, height)
-    cDC.SelectObject(dataBitMap)
-    cDC.BitBlt((0,0),(width, height) , dcObj, (x,y), win32con.SRCCOPY)
-    dataBitMap.SaveBitmapFile(cDC, bmpfilenamename)
+    screenshot = pyautogui.screenshot(region=(x, y, width, height))
+    screenshot = screenshot.resize((224, 224), Image.NEAREST)
+    #save_screenshot(screenshot)#UNCOMMENT ONLY WHEN YOU NEED TO COLLECT YOUR SCREENSHOTS
 
-    image = Image.open(bmpfilenamename)
-    image = image.resize((224*2, 224*2), Image.NEAREST)
-    image.save(result)
-    dcObj.DeleteDC()
-    cDC.DeleteDC()
-    os.remove(bmpfilenamename)
-    win32gui.ReleaseDC(hwnd, wDC)
-    win32gui.DeleteObject(dataBitMap.GetHandle())
-    #return pyautogui.screenshot(region=(x, y, width, height))
+    img_array = np.expand_dims(screenshot, 0)
+    predictions = model.predict(img_array)
+    score = np.exp(predictions[0])/sum(np.exp(predictions[0]))
+    result = class_names[np.argmax(score)]
+
+    log("result from model: {}".format(result))
     return result
+
+def save_screenshot(screenshot):
+    actual_time = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')
+    file_name = actual_time + ".jpg"
+    log("take screenshot, file name: ".format(file_name))
+    screenshot.save(r"G:\new-world-bot\saved_data\screenshots\\"+ file_name)
