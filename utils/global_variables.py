b9e1564adfc856793a7668aef2fa4ac22c257ace
Siterizer <siterizer@gmail.com>
2021-09-14 11:54:11 +0200

    global_variables/imports refactoring

--- utils/global_variables.py
+++ utils/global_variables.py
@@ -8,40 +8,7 @@ CONFIG_PATH = os.path.join(ROOT_DIR, 'config.yml')
 MODEL_PATH = os.path.join(ROOT_DIR, 'model\\')
 SCREENSHOTS_PATH = os.path.join(ROOT_DIR, '..\saved_data\screenshots\\')
 
-
-def init_variables():
-    global root
-    root = Tk()
-
-    global continue_fishing
-    continue_fishing = False
-
-    global last_results
-    last_results = LastResults()
-    
-    global last_repair_time
-    last_repair_time = int(time.time())
-
-def get_root():
-    global root
-    return root
-
-def fishing_state():
-    global continue_fishing
-    return continue_fishing
-
-def negate_fishing_state():
-    global continue_fishing
-    continue_fishing = not continue_fishing
-
-def results():
-    global last_results
-    return last_results
-    
-def get_last_repair_time():
-    global last_repair_time
-    return last_repair_time
-    
-def update_last_repair_time():
-    global last_repair_time
-    last_repair_time = int(time.time())
+continue_fishing = False
+last_repair_time = int(time.time())
+last_results = LastResults()
+root = Tk()
