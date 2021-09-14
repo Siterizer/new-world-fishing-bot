92ed4f9e64c249a7ad3909e99ad525ee489a4ae9
Siterizer <siterizer@gmail.com>
2021-09-14 13:39:46 +0200

    split gui_initializer into separate files

---
+++ gui/repairing_column/components/every.py
@@ -0,0 +1,14 @@
+from tkinter import Label, LabelFrame, Entry
+from utils.config import dict
+
+
+
+def repairing_column_every(repairing_column):
+    repairing_column_every_container = LabelFrame(repairing_column)
+    repairing_column_every_container.grid(row=3, column=0, pady=(5, 5))
+    repairing_column_every_text_left = Label(repairing_column_every_container, text = "Repair every:")
+    repairing_column_every_text_left.grid(row=0, column=0, padx=(0, 5))
+    repairing_column_every_entry = Entry(repairing_column_every_container, width=4, textvariable=dict['repairing']['every'])
+    repairing_column_every_entry.grid(row=0, column=1, padx=(41, 0))
+    repairing_column_every_text_right = Label(repairing_column_every_container, text = "s")
+    repairing_column_every_text_right.grid(row=0, column=3, padx=(0, 6))
---
+++ gui/start_fishing_button/start_fishing_button.py
@@ -0,0 +1,13 @@
+import utils.global_variables as gv
+import utils.global_variables as gv
+from tkinter import Button, LabelFrame
+from functools import partial
+from gui.gui_functions import start_fishing
+
+
+def start_fishing_button():
+    start_fishing_container = LabelFrame(gv.root)
+    start_fishing_container.grid(row=3, columnspan=2, padx=(10, 0), pady=(15, 0))
+    start_fishing_button = Button(start_fishing_container, text = "Start fishing", font=18)
+    start_fishing_button.configure(command = partial(start_fishing, start_fishing_button))
+    start_fishing_button.grid(row=0, column=0)
