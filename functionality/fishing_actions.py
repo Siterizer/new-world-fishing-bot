81c06e97d5d7819dcee4a1b45e8020ec33421e9f
Siterizer <siterizer@gmail.com>
2021-09-14 12:45:52 +0200

    extract gui functions into 'gui_functions.py', create fishing_loop file, refactoring in fishing_actions

--- functionality/fishing_actions.py
+++ functionality/fishing_actions.py
@@ -1,81 +1,5 @@
-import utils.global_variables as gv
 from utils.config import dict
-from wrappers.model_wrapper import get_model_result
 from wrappers.pyautogui_wrapper import *
-
-
-
-def fishing_loop():
-    gv.last_results.add(fishing())
-    if(gv.last_results.is_full_of('0')):
-        if(dict['repairing']['enable'].get() == 1):
-            if(int(time.time()) > gv.last_repair_time+ dict['repairing']['every'].get()):
-                gv.last_repair_time = int(time.time())
-                repairing()
-    if (gv.continue_fishing):
-        gv.root.after(10, fishing_loop)
-
-
-def repairing():
-    print('repair')
-    #End fishing cycle
-    time.sleep(1)
-    press_key('f3')
-    release_key('f3')
-    time.sleep(1)
-
-    #Open inventory
-    press_key('tab')
-    release_key('tab')
-    time.sleep(3)
-
-    #Press 'r' and click on fishing rod
-    press_key('r')
-    time.sleep(1)
-    click_mouse_with_coordinates(dict['repairing']['x'].get(), dict['repairing']['y'].get(), 0)
-    time.sleep(0.1)
-    release_key('r')
-
-    #Confirm repair
-    time.sleep(1)
-    press_key('e')
-    release_key('e')
-    time.sleep(1)
-
-    #Close inventory
-    press_key('escape')
-    release_key('escape')
-
-    #Grab again fishing rod
-    time.sleep(1)
-    press_key('f3')
-    release_key('f3')
-
-def fishing():
-    screenshot = get_screenshot(dict['fishing']['x'].get(), dict['fishing']['y'].get(),
-                                dict['fishing']['width'].get(), dict['fishing']['height'].get())
-    #save_screenshot(screenshot) #UNCOMMENT ONLY WHEN YOU NEED TO SAVE YOUR SCREENSHOTS (model training etc.)
-    print(gv.last_results.data)
-    result_from_model = get_model_result(screenshot)
-    if(gv.last_results.get_last_value() != result_from_model and result_from_model != '1' ):
-        return result_from_model
-    if result_from_model == '0': # 0 - model does not match any data (not fish captured yet)
-        print("Waiting for fish...")
-        return '0'
-    elif result_from_model == '1': # 1 - model noticed a fish(left click to initiate fishing)
-        print("Found a fish!")
-        reel_fish()
-        return '1'
-    elif result_from_model == '2': #2 - model matched the green icon (reeling a fish in)
-        print("Reeling a fish")
-        reel_fish()
-        return '2'
-    elif result_from_model == '3': #3 - model matched the yellow/red icon (wait 2sec)
-        print("Pause fishing")
-        initiate_game()
-        time.sleep(2)
-        return '3'
-
 
 def initiate_game():
     press_mouse_key()
@@ -86,3 +10,40 @@ def reel_fish():
     press_mouse_key()
     time.sleep(1.5)
     release_mouse_key()
+
+def repairing():
+    print('repair')
+    arm_disarm_fishing_rod()
+    open_close_inventory()
+    repair()
+    confirm_repair()
+    open_close_inventory()
+    arm_disarm_fishing_rod()
+
+def arm_disarm_fishing_rod():
+    time.sleep(1)
+    press_key('f3')
+    release_key('f3')
+    time.sleep(1)
+
+def open_close_inventory():
+    time.sleep(1)
+    press_key('tab')
+    release_key('tab')
+    time.sleep(1)
+
+def repair():
+    time.sleep(1)
+    press_key('r')
+    time.sleep(0.1)
+    click_mouse_with_coordinates(dict['repairing']['x'].get(), dict['repairing']['y'].get())
+    time.sleep(0.1)
+    release_key('r')
+    time.sleep(1)
+
+def confirm_repair():
+    time.sleep(1)
+    press_key('e')
+    time.sleep(0.1)
+    release_key('e')
+    time.sleep(1)
