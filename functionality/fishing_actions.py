82d64b0ece1b34512d8f8c77d12e0af19cf35c94
Siterizer <siterizer@gmail.com>
2021-09-16 18:54:06 +0200

    add logging module

--- functionality/fishing_actions.py
+++ functionality/fishing_actions.py
@@ -1,27 +1,36 @@
 from utils.config import dict
 from time import sleep
 from wrappers.pyautogui_wrapper import *
+from wrappers.logging_wrapper import debug
 
 def fish_notice():
+    debug("Press mouse key for: {} s".format(dict['fishing']['timeouts']['notice']))
     press_mouse_key()
     sleep(dict['fishing']['timeouts']['notice'])
     release_mouse_key()
 
 def reel_fish():
+    debug("Press mouse key for: {} s".format(dict['fishing']['timeouts']['reeling']))
     press_mouse_key()
     sleep(dict['fishing']['timeouts']['reeling'])
     release_mouse_key()
 
 def pause():
+    debug("Pause for: {} s".format(dict['fishing']['timeouts']['pause']))
     sleep(dict['fishing']['timeouts']['pause'])
 
 def repairing():
-    print('repair')
+    debug("Disarm fishing rod. Total time: {} s".format(dict['repairing']['timeouts']['arm_disarm']))
     arm_disarm_fishing_rod()
+    debug("Open inventory. Total time: {} s".format(dict['repairing']['timeouts']['inventory']))
     open_close_inventory()
+    debug("Repair fishing rod. Total time: {} s".format(dict['repairing']['timeouts']['repair']))
     repair()
+    debug("Confirm repair. Total time: {} s".format(dict['repairing']['timeouts']['confirm']))
     confirm_repair()
+    debug("Close inventory. Total time: {} s".format(dict['repairing']['timeouts']['confirm']))
     open_close_inventory()
+    debug("Arm fishing rod. Total time: {} s".format(dict['repairing']['timeouts']['confirm']))
     arm_disarm_fishing_rod()
 
 def arm_disarm_fishing_rod():
