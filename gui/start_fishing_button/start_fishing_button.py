2c569d2a8943963775d3e871bd11b1dda616454f
DaGuT <mangustalex11@gmail.com>
2021-10-02 17:16:24 +0300

    centered start fishing button

--- gui/start_fishing_button/start_fishing_button.py
+++ gui/start_fishing_button/start_fishing_button.py
@@ -7,7 +7,7 @@ from gui.gui_functions import start_fishing
 
 def start_fishing_button():
     start_fishing_container = LabelFrame(gv.root)
-    start_fishing_container.grid(row=3, columnspan=2, padx=(10, 0), pady=(15, 0))
+    start_fishing_container.grid(row=3, columnspan=4, padx=(10, 0), pady=(15, 0))
     start_fishing_button = Button(start_fishing_container, text = "Start fishing", font=18)
     start_fishing_button.configure(command = partial(start_fishing, start_fishing_button))
     start_fishing_button.grid(row=0, column=0)
