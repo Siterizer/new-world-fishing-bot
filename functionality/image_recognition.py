646cc7f0dbcdac65e5312e12bfc4d9b84da60917
Alexander Saynisch <alexander@saynisch.de>
2021-10-13 13:54:44 +0200

    Optimized Color Searching with opencv/Numpy functions

--- functionality/image_recognition.py
+++ functionality/image_recognition.py
@@ -1,6 +1,7 @@
 from utils.config import dict
 from numpy import array
 import cv2 as cv
+import numpy as np
 from PIL import ImageGrab
 from utils.global_variables import WAITING_FOR_FISH, FISH_NOTICED
 
@@ -13,28 +14,29 @@ COLOR_WAGES = 7
 
 def image_recognition_result(x, y, width, height):
     region=(x, y, x + width, y + height)
-    img = ImageGrab.grab(bbox = region)
-    img_cv = cv.cvtColor(array(img), cv.COLOR_RGB2BGR)
+    img = array(ImageGrab.grab(bbox = region))
+    img_cv = cv.cvtColor(img, cv.COLOR_RGB2BGR)
     res = cv.matchTemplate(img_cv, NOTICE, eval('cv.TM_CCOEFF_NORMED'))
     if((res >= 0.7).any()):
         return '1'
     res = cv.matchTemplate(img_cv, NOTHING, eval('cv.TM_CCOEFF_NORMED'))
     if((res >= 0.7).any()):
         return '0'
-
-    for i in range(width):
-        for j in range(height):
-            color = img.getpixel((i, j))
-            if pixel_match(color, REEL_COLOR):
-                return '2'
-            if pixel_match(color, WAIT_COLOR_BROWN):
-                return '3'
-            if pixel_match(color, WAIT_COLOR_RED):
-                return '4'
+    
+    if pixel_match(img, REEL_COLOR):     
+        return '2'
+    if pixel_match(img, WAIT_COLOR_BROWN):        
+        return '3'
+    if pixel_match(img, WAIT_COLOR_RED):                    
+        return '4'           
     return '5'
 
-def pixel_match(color, matcher):
-    for i in range (0,3):
-        if not((matcher[i] - COLOR_WAGES) <= color[i] <= (matcher[i] + COLOR_WAGES)):
-            return False
-    return True
+def pixel_match(img, matcher):
+    lower, upper = ([matcher[0] - COLOR_WAGES, matcher[1] - COLOR_WAGES,  matcher[2] - COLOR_WAGES], [matcher[0] + COLOR_WAGES, matcher[1] + COLOR_WAGES,  matcher[2] + COLOR_WAGES])
+    color_lower = array( lower)
+    color_upper = array( upper)
+
+    mask = cv.inRange(img, color_lower, color_upper)
+    if mask.any():
+        return True
+    return False
