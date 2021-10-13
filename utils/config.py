2044de2da86c51053348ef9707549a9a1a52ca45
Siterizer <siterizer@gmail.com>
2021-10-13 16:28:31 +0200

    shift random.uniform to random.normal for all random_timeouts

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,7 @@
 from yaml import safe_load, dump
 from tkinter import IntVar
 from utils.global_variables import CONFIG_PATH
-import random
+from numpy import random
 
 
 config = safe_load(open(CONFIG_PATH))
@@ -190,4 +190,12 @@ def save_data():
         dump(d, yaml_file, sort_keys=False)
 
 def random_timeout(key):
-    return round(random.uniform(key['min'], key['max']),2)
+    upper_limit = key['max']
+    lower_limit = key['min']
+
+    loc = (upper_limit + lower_limit) / 2
+    scale = (upper_limit - lower_limit) / 4
+
+    sample = random.normal(loc, scale)
+
+    return round(min(max(sample, lower_limit), upper_limit),2)
