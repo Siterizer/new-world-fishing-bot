78c667d47b5ff43d817f096fecd1a9fc692c7a49
0xFFFF <43180177+innellea@users.noreply.github.com>
2021-10-14 10:42:40 +0100

    resolving invalid escape sequence
    resolving invalid escape sequence '\c'

--- utils/global_variables.py
+++ utils/global_variables.py
@@ -4,14 +4,16 @@ import sys
 from time import time
 from tkinter import Tk
 
+
 def rootPath():
     try:
         return sys._MEIPASS
     except Exception:
         return path.abspath(".")
 
-ROOT_DIR  = rootPath()
-CONFIG_PATH = path.join(ROOT_DIR, 'resources\config.yml')
+
+ROOT_DIR = rootPath()
+CONFIG_PATH = path.join(ROOT_DIR, 'resources\\config.yml')
 WAITING_FOR_FISH = path.join(ROOT_DIR, 'resources\\waiting_for_fish.jpg')
 FISH_NOTICED = path.join(ROOT_DIR, 'resources\\fish_noticed.jpg')
 ICON_PATH = path.join(ROOT_DIR, 'resources\\icon.ico')
