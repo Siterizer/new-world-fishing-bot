2a2087f3ccb9478c1586779507adde73e7e3dd25
Alexander Saynisch <alexander@saynisch.de>
2021-10-15 14:37:32 +0200

    add variable resize of templates

--- functionality/image_recognition.py
+++ functionality/image_recognition.py
@@ -3,13 +3,43 @@ from numpy import array
 import cv2 as cv
 from PIL import ImageGrab
 from utils.global_variables import WAITING_FOR_FISH, FISH_NOTICED
+from os import path
+import configparser
+
+#Baseline Resolution at which the templates were recorded
+base_width = 1920
+base_height = 1080
+#Get Game Resolution from user_preload_settings.cfg
+nw_config_path = path.expandvars(r'%APPDATA%\AGS\New World\user_preload_settings.cfg')
+if path.isfile(nw_config_path):
+    config = configparser.ConfigParser(allow_no_value=True)
+    with open(nw_config_path, 'r') as f:
+        config_string = '[dummy_section]\n' + f.read()
+    config.read_string(config_string)
+    scaleX = int(config['dummy_section']["r_Width"]) / base_width
+    scaleY = int(config['dummy_section']["r_Height"]) / base_height
+    print(scaleX)
+else:
+  scaleX = 1
+  scaleY = 1
 
 NOTHING = cv.imread(WAITING_FOR_FISH)
+width = int(NOTHING.shape[1] * scaleX)
+height = int(NOTHING.shape[1] * scaleY)
+dim = (width, height)  
+NOTHING = cv.resize(NOTHING, dim, cv.INTER_LINEAR)
 NOTICE = cv.imread(FISH_NOTICED)
+width = int(NOTICE.shape[1] * scaleX)
+height = int(NOTICE.shape[1] * scaleY)
+dim = (width, height)  
+NOTICE = cv.resize(NOTICE, dim, cv.INTER_LINEAR)
 REEL_COLOR = dict['colors']['green']
 WAIT_COLOR_BROWN = dict['colors']['brown']
 WAIT_COLOR_RED = dict['colors']['red']
 COLOR_WAGES = 7
+
+
+
 
 def image_recognition_result(x, y, width, height):
     region=(x, y, x + width, y + height)
