62a409cda1e388b77caf56c4103fa8aef71db054
jake <jgarrison@hoganassessments.com>
2021-10-21 12:30:12 -0500

    randomizes title

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import utils.global_variables as gv
 from gui.gui_initializer import gui_init
+import random
+import string
 
 gui_init()
 gv.root.iconbitmap(gv.ICON_PATH)
-gv.root.title('fishing bot')
+gv.root.title(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10)))
 gv.root.mainloop()
