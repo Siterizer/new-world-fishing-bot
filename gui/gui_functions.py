e0550a424d64c9ec77318a6cef9065fb7a287ee5
0xFFFF <43180177+innellea@users.noreply.github.com>
2021-10-22 22:08:42 +0100

    add flexible resolution settings

--- gui/gui_functions.py
+++ gui/gui_functions.py
@@ -11,17 +11,23 @@ def popup_rectangle_window(button, x, y, width, height):
     window.attributes('-fullscreen', True)
     window.wm_attributes('-transparentcolor', window['bg'])
     canvas = Canvas(window, width=10000, height=10000)
-    canvas.create_rectangle(x.get(), y.get(), x.get()+width.get(), y.get()+height.get(), outline="green", width=5)
+    canvas.create_rectangle(x.get(), y.get(), x.get(
+    )+width.get(), y.get()+height.get(), outline="green", width=5)
     canvas.pack()
-    button.configure(command = partial(destroy_rectangle_window, window, button, x, y, width, height))
+    button.configure(command=partial(destroy_rectangle_window,
+                     window, button, x, y, width, height))
+
 
 def destroy_rectangle_window(window, button, x, y, width, height):
     window.destroy()
-    button.configure(command = partial(popup_rectangle_window, button, x,y,width,height))
+    button.configure(command=partial(
+        popup_rectangle_window, button, x, y, width, height))
+
 
 def on_closing():
     save_data()
     gv.root.destroy()
+
 
 def change_repair_button_state(button):
     if (dict['repairing']['enable'].get() == 1):
@@ -33,6 +39,7 @@ def change_repair_button_state(button):
         button.configure(bg="green")
         dict['repairing']['enable'] = IntVar(value=1)
 
+
 def change_bait_button_state(button):
     if (dict['bait']['enable'].get() == 1):
         button.configure(text="OFF")
@@ -43,17 +50,17 @@ def change_bait_button_state(button):
         button.configure(bg="green")
         dict['bait']['enable'] = IntVar(value=1)
 
+
 def changeFishingState(button):
     gv.continue_fishing = not gv.continue_fishing
     if(gv.continue_fishing):
-        button.configure(text = "Stop fishing")
-        button.configure(command = partial(changeFishingState, button))
+        button.configure(text="Stop fishing")
+        button.configure(command=partial(changeFishingState, button))
         return
-    button.configure(text = "Start fishing")
-    button.configure(command = partial(start_fishing, button))
+    button.configure(text="Start fishing")
+    button.configure(command=partial(start_fishing, button))
+
 
 def start_fishing(button):
     changeFishingState(button)
     fishing_loop()
-
-
