18dbc18cf7bbfb4d7309ec3a3a50d0064503563e
Rhino <6531393+srhinos@users.noreply.github.com>
2021-10-27 16:51:41 -0700

    Use New Context Dict in Win Wrapper

--- wrappers/win32api_wrapper.py
+++ wrappers/win32api_wrapper.py
@@ -5,15 +5,15 @@ import win32api
 import win32con
 
 
-async def click_mouse_with_coordinates(loop, x, y):
+async def click_mouse_with_coordinates(ctx, x, y):
     with concurrent.futures.ThreadPoolExecutor() as pool:
-        await loop.run_in_executor(pool, win32api.SetCursorPos, (x, y))
+        await ctx["loop"].run_in_executor(pool, win32api.SetCursorPos, (x, y))
         await sleep(0.02)
-        await loop.run_in_executor(
+        await ctx["loop"].run_in_executor(
             pool, win32api.mouse_event, win32con.MOUSEEVENTF_LEFTDOWN, 0, 0
         )
         await sleep(0.05)
-        await loop.run_in_executor(
+        await ctx["loop"].run_in_executor(
             pool, win32api.mouse_event, win32con.MOUSEEVENTF_LEFTUP, 0, 0
         )
 
@@ -29,28 +29,28 @@ VK_CODE = {
 }
 
 
-async def press_key(loop, key):
+async def press_key(ctx, key):
     with concurrent.futures.ThreadPoolExecutor() as pool:
-        await loop.run_in_executor(pool, win32api.keybd_event, VK_CODE[key], 0, 0, 0)
+        await ctx["loop"].run_in_executor(pool, win32api.keybd_event, VK_CODE[key], 0, 0, 0)
         await sleep(0.05)
 
 
-async def release_key(loop, key):
+async def release_key(ctx, key):
     with concurrent.futures.ThreadPoolExecutor() as pool:
-        await loop.run_in_executor(
+        await ctx["loop"].run_in_executor(
             pool, win32api.keybd_event, VK_CODE[key], 0, win32con.KEYEVENTF_KEYUP, 0
         )
 
 
-async def press_mouse_key(loop):
+async def press_mouse_key(ctx):
     with concurrent.futures.ThreadPoolExecutor() as pool:
-        await loop.run_in_executor(
+        await ctx["loop"].run_in_executor(
             pool, win32api.mouse_event, win32con.MOUSEEVENTF_LEFTDOWN, 0, 0
         )
 
 
-async def release_mouse_key(loop):
+async def release_mouse_key(ctx):
     with concurrent.futures.ThreadPoolExecutor() as pool:
-        await loop.run_in_executor(
+        await ctx["loop"].run_in_executor(
             pool, win32api.mouse_event, win32con.MOUSEEVENTF_LEFTUP, 0, 0
         )
